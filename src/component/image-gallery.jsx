845c36cfeb5cf9765f73a6e29752a5bbaa3b983d
Mohammadshuvouup <mohammad.ali.shuvo161@gmail.com>
2023-11-03 00:17:25 +0600

    fixed image heights

--- src/component/image-gallery.jsx
+++ src/component/image-gallery.jsx
@@ -1,68 +1,11 @@
 import { useState, useEffect } from "react";
 import SortableList, { SortableItem } from 'react-easy-sort'
 import {arrayMoveImmutable} from 'array-move';
+import data from '../data/data.json';
 
 const ImageGallery = () => {
   const [images, setImages] = useState([]);
   const [selectedItem, setSelectedItem] = useState([]);
-
-  const data = [
-    {
-      id: "1",
-      name: "image1",
-      image: "/assets/images/image-1.webp",
-    },
-    {
-      id: "2",
-      name: "image2",
-      image: "/assets/images/image-2.webp",
-    },
-    {
-      id: "3",
-      name: "image3",
-      image: "/assets/images/image-3.webp",
-    },
-    {
-      id: "4",
-      name: "image4",
-      image: "/assets/images/image-4.webp",
-    },
-    {
-      id: "5",
-      name: "image5",
-      image: "/assets/images/image-5.webp",
-    },
-    {
-      id: "6",
-      name: "image6",
-      image: "/assets/images/image-6.webp",
-    },
-    {
-      id: "7",
-      name: "image7",
-      image: "/assets/images/image-7.webp",
-    },
-    {
-      id: "8",
-      name: "image8",
-      image: "/assets/images/image-8.webp",
-    },
-    {
-      id: "9",
-      name: "image9",
-      image: "/assets/images/image-9.webp",
-    },
-    {
-      id: "10",
-      name: "image10",
-      image: "/assets/images/image-10.jpeg",
-    },
-    {
-      id: "11",
-      name: "image11",
-      image: "/assets/images/image-11.jpeg",
-    },
-  ];
 
   useEffect(() => {
     setImages(data);
