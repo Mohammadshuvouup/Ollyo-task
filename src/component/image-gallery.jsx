f1374cd5df405417ed79166c57489265fcda1802
Mohammadshuvouup <mohammad.ali.shuvo161@gmail.com>
2023-11-02 16:50:02 +0600

    Show/hide image-gallery-header texts

--- src/component/image-gallery.jsx
+++ src/component/image-gallery.jsx
@@ -1,99 +1,120 @@
 import { useState, useEffect } from "react";
 const ImageGallery = () => {
-    const [images, setImages] = useState([]);
-    const [selectedItem, setSelectedItem] = useState([]);
+  const [images, setImages] = useState([]);
+  const [selectedItem, setSelectedItem] = useState([]);
 
   const data = [
     {
-        id: "1",
-        name: "image1",
-        image: "/assets/images/image-1.webp",
+      id: "1",
+      name: "image1",
+      image: "/assets/images/image-1.webp",
     },
     {
-        id: "2",
-        name: "image2",
-        image: "/assets/images/image-2.webp",
+      id: "2",
+      name: "image2",
+      image: "/assets/images/image-2.webp",
     },
     {
-        id: "3",
-        name: "image3",
-        image: "/assets/images/image-3.webp",
+      id: "3",
+      name: "image3",
+      image: "/assets/images/image-3.webp",
     },
     {
-        id: "4",
-        name: "image4",
-        image: "/assets/images/image-4.webp",
+      id: "4",
+      name: "image4",
+      image: "/assets/images/image-4.webp",
     },
     {
-        id: "5",
-        name: "image5",
-        image: "/assets/images/image-5.webp",
+      id: "5",
+      name: "image5",
+      image: "/assets/images/image-5.webp",
     },
     {
-        id: "6",
-        name: "image6",
-        image: "/assets/images/image-6.webp",
+      id: "6",
+      name: "image6",
+      image: "/assets/images/image-6.webp",
     },
     {
-        id: "7",
-        name: "image7",
-        image: "/assets/images/image-7.webp",
+      id: "7",
+      name: "image7",
+      image: "/assets/images/image-7.webp",
     },
     {
-        id: "8",
-        name: "image8",
-        image: "/assets/images/image-8.webp",
+      id: "8",
+      name: "image8",
+      image: "/assets/images/image-8.webp",
     },
     {
-        id: "9",
-        name: "image9",
-        image: "/assets/images/image-9.webp",
+      id: "9",
+      name: "image9",
+      image: "/assets/images/image-9.webp",
     },
     {
-        id: "10",
-        name: "image10",
-        image: "/assets/images/image-10.jpeg",
+      id: "10",
+      name: "image10",
+      image: "/assets/images/image-10.jpeg",
     },
     {
-        id: "11",
-        name: "image11",
-        image: "/assets/images/image-11.jpeg",
+      id: "11",
+      name: "image11",
+      image: "/assets/images/image-11.jpeg",
     },
-  
   ];
 
   useEffect(() => {
-      setImages(data);
-  },[]);
- 
-  const updateSelectedItem =(item) =>{
-    if(!isDuplicate(item)){
-        setSelectedItem([...selectedItem , item]);
-    }
-    else{
-     const updated_array = selectedItem.filter((value)=>  value.id!==item.id );
-     setSelectedItem(updated_array);
-    }
-  }
+    setImages(data);
+  }, []);
 
-  const isDuplicate =(item)=>{
-   return selectedItem.includes(item);
+  const updateSelectedItem = (item) => {
+    if (!isDuplicate(item)) {
+      setSelectedItem([...selectedItem, item]);
+    } else {
+      const updated_array = selectedItem.filter(
+        (value) => value.id !== item.id
+      );
+      setSelectedItem(updated_array);
+    }
   };
 
-  const deleteItem =()=>{
+  const isDuplicate = (item) => {
+    return selectedItem.includes(item);
+  };
+
+  const deleteItem = () => {
     const newData = images.filter((value) => !selectedItem.includes(value));
-          setImages(newData);
-          setSelectedItem([]);
+    setImages(newData);
+    setSelectedItem([]);
   };
 
   return (
     <div className="image-gallery">
       <div className="image-gallery__header">
         <div className="select-file">
-             <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-             <label htmlFor="vehicle1">{selectedItem.length} File Selected</label>
+          {selectedItem.length == 0 ? (
+            <p>Gallery</p>
+          ) : (
+            <>
+              <input
+                type="checkbox"
+                id="files"
+                name="files"
+                value="Bike"
+                checked
+                readOnly
+              />
+              <label htmlFor="files">
+                {selectedItem.length} File Selected
+              </label>
+            </>
+          )}
         </div>
-        <div  onClick={() =>deleteItem()} className="delete-btn">Delete All</div>
+        {selectedItem.length == 0 ? (
+          ""
+        ) : (
+          <div onClick={() => deleteItem()} className="delete-btn">
+            Delete All
+          </div>
+        )}
       </div>
       <div className="image-gallery__body">
         <div className="image-gallery__images">
@@ -101,10 +122,17 @@ const ImageGallery = () => {
             return (
               <div className="image" key={key}>
                 <img src={item.image} alt={item.name} />
-                <label 
-                onClick={()=> updateSelectedItem(item)}
-                className="overlay" htmlFor={item.id}/>
-                <input type="checkbox" id={item.id} name={item.name} value={item.name}/>
+                <label
+                  onClick={() => updateSelectedItem(item)}
+                  className="overlay"
+                  htmlFor={item.id}
+                />
+                <input
+                  type="checkbox"
+                  id={item.id}
+                  name={item.name}
+                  value={item.name}
+                />
               </div>
             );
           })}
